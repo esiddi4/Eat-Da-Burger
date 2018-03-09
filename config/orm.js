6b8fb89e8eaaf1601ad6f20ff7a7961ef80977c1
Enas Siddiqi <enassiddiqi@Enass-MacBook-Pro.local>
2018-03-08 20:15:58 -0600

    ORM and model complete

--- config/orm.js
+++ config/orm.js
@@ -5,14 +5,22 @@ var orm = {
     selectAll: function (table, cb) {
         var query = "SELECT ALL FROM * " + table + ";";
         connection.query(query, function(err, result){
-            if (err) { throw err; }
-            cb(result)
+            if (err) throw err;
+            cb(result);
         });
     },
-    insertOne: function (cb) {
-
+    insertOne: function (table, col, val, cb) {
+        var query = "INSERT INTO " + table + " (" + col + ") VALUES (" + val + ");";
+        connection.query(query, function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
     },
-    updateOne: function (cb) {
+    // An example of colVal would be 'devoured=true'
+    updateOne: function (table, col, val, condition, cb) {
+        var query = "UPDATE " + table + " SET " + col + "=" + val + " WHERE " + condition;
 
     }
 }
+
+module.exports = orm;
